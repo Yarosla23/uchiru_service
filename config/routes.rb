eee648435a77dc25de9531fc01a8d9728909a790
Yaroslav Andreychuk <yaroslav.andreychuk@jetruby.com>
2025-08-18 23:12:29 +0300

    Настроены маршруты для SchoolClasses и Students

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
- namespace :api do
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
+
+  namespace :api do
     namespace :v1 do
       resources :students, only: [ :create, :destroy ]
 
       resources :schools, only: [] do
-        resources :classes, only: [ :index ] do
-          resources :students, only: [ :index ], controller: "class_students"
+        resources :school_classes, path: "classes", only: :index do
+          resources :students, only: :index
         end
       end
     end
